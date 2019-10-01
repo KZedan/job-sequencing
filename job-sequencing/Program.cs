298ef8a9f830c05432d283f51b684bc2e3eae33f
Kareem <kareem335@yahoo.co.uk>
2019-10-01 14:12:54 +0100

    first commit

---
+++ job-sequencing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace job_sequencing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
